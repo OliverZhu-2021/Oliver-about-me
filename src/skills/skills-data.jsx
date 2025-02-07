c68a74c75522821e7a0186e96a0e90778691d1b2
Hao Zhu <zhuhao_0227@sina.com>
2025-02-07 09:15:21 +0800

    Modify skill bar

--- src/skills/skills-data.jsx
+++ src/skills/skills-data.jsx
@@ -2,66 +2,38 @@ const skills = {
   1: [
     {
       title: 'HTML',
-      level: 6,
-    },
-    {
-      title: 'CSS',
-      level: 5,
-    },
-    {
-      title: 'SASS',
-      level: 2,
-    },
-    {
-      title: 'JavaScript',
-      level: 5,
-    },
-    {
-      title: 'TypeScript',
       level: 4,
     },
     {
+      title: 'CSS',
+      level: 3,
+    },
+    {
+      title: 'JavaScript',
+      level: 3,
+    },
+    {
       title: 'Tailwind CSS',
-      level: 1,
+      level: 2,
     },
     {
       title: 'React JS',
       level: 3,
     },
-    {
-      title: 'Figma',
-      level: 4,
-    },
   ],
   2: [
     {
       title: 'PostgreSQL',
-      level: 5,
-    },
-    {
-      title: 'Node.Js',
-      level: 4,
-    },
-    {
-      title: 'MongoDB',
-      level: 2,
+      level: 3,
     },
     {
       title: 'Git',
-      level: 4,
-    },
-    {
-      title: 'PHP',
-      level: 1,
+      level: 2,
     },
     {
       title: 'SQL',
-      level: 3
+      level: 2
     },
-    {
-      title: 'Express.js',
-      level: 3,
-    }
   ],
 }
 
--- src/skills/skills-menu.jsx
+++ src/skills/skills-menu.jsx
@@ -42,7 +42,7 @@ const SkillsMenu = () => {
             >
               <h3>{skill.title}</h3>
               <div className='level-container'>
-                {[...Array(6)].map((_, i) => (
+                {[...Array(5)].map((_, i) => (
                   <div
                     key={i}
                     className={`level-point ${
