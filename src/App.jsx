fb2e152930f38536fdbd8956990ea4b45a51c943
Hao Zhu <zhuhao_0227@sina.com>
2025-02-08 10:16:37 +0800

    deploy setting for react-router

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
   
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <GlobalStyle />
         <Nav />
         <AnimeBg />
